b5c33dd40f9719053729e49399140df8fa5b3918
Forever <109850034+whwhwh0715@users.noreply.github.com>
2024-08-21 23:22:46 +0800

    Update action.js

--- action.js
+++ action.js
@@ -1,6 +1,1 @@
-function showName() {
-    var url = document.getElementById("url").value;
-    var new_url = 'https://jx.xmflv.cc/?url=' + url;
-    console.log("新的链接是：" + new_url);
-    window.open(new_url);
-  }
+
